40786b93a61397984ebf6fb9e3ac4336d750c09e
citylife4 <delfimvalverde@gmail.com>
2015-10-14 00:43:50 +0100

    "second submission"

--- src.c
+++ src.c
@@ -501,6 +501,7 @@ int main (int argc, char** argv) {
 	
 	printf("opening llopen with %d\n", txrx);
 	rs = llopen(fd, txrx);
+	linkLayer.fd = rs;
 	printf("result : %d\n", rs);
 	
 	if(txrx == TRANSMITTER)
